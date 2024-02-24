d13a954334d97cfbf50793850a062805d3b433f5
Dilip7998 <139032366+Dilip7998@users.noreply.github.com>
2024-02-24 22:23:05 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <>
+    <h1 className='text-2xl text-center text-red-900'>Welcome in Our Hostel Management Website</h1>
+    <div className=" border-2 border-red-500 h-44 w-48"> 
+    <div className="border-3 border-green-200 h-32 w-32">
+      </div>
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
